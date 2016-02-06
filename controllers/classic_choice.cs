32836ffed43b8203be645353808ed0405acae2aa
Pazaak <aggalem@gmail.com>
2016-02-06 19:54:39 +0100

    Classical choices (typical rpg stuff) implemented

--- controllers/classic_choice.cs
+++ controllers/classic_choice.cs
@@ -2,29 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Dogenova.logic;
 
 namespace Dogenova.controllers
 {
     class classic_choice : controller
     {
-        public classic_choice(bool con1)
-        {
+        private bool user;
+        private bool con1;
 
+        public classic_choice(bool con1_)
+        {
+            this.user = false;
+            this.con1 = con1_;
         }
 
         public bool isUser()
         {
-            throw new NotImplementedException();
+            return this.user;
         }
 
         public bool isCon1()
         {
-            throw new NotImplementedException();
+            return this.con1;
         }
 
-        public phases getChoice(battler[] a, int[] b)
+        public phases getChoice(battler[] btls, int[] tiers)
         {
-            throw new NotImplementedException();
+            bool frontTargets = false;
+            for (int i = con1 ? 4 : 0; i < (con1 ? 8 : 4); i++)
+            {
+                if (!btls[i].dead && btls[i].front) frontTargets = true;
+            }
+
+            phases output = new phases();
+
+            for (int i = con1 ? 0 : 4; i < (con1 ? 4 : 8); i++)
+            {
+                if (!btls[i].dead)
+                {
+                    int target = classicFunctions.killableTarget(btls, i, con1, frontTargets);
+
+                    if (target != -1)
+                    {
+                        // Attack target
+                        output.combat[tiers[i]].Add(Constants.F_ATTACK);
+                        output.combat[tiers[i]].Add(i);
+                        output.combat[tiers[i]].Add(target);
+                    }
+                    else if (classicFunctions.criticalLife(btls[i]))
+                    {
+                        //Defend
+                        output.pre_combat[tiers[i]].Add(Constants.F_DEFEND);
+                        output.pre_combat[tiers[i]].Add(i);
+                        output.post_combat[tiers[i]].Add(Constants.F_DEFEND);
+                        output.post_combat[tiers[i]].Add(i);
+                    }
+                    else
+                    {
+                        // Attack the target that will receive the greatest damage
+                        target = classicFunctions.greatestDamageTarget(btls, i, con1, frontTargets);
+                        output.combat[tiers[i]].Add(Constants.F_ATTACK);
+                        output.combat[tiers[i]].Add(i);
+                        output.combat[tiers[i]].Add(target);
+
+                    }
+                }
+            }
+
+            return output;
         }
     }
 }
---
+++ logic/classicFunctions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dogenova.logic
+{
+    static class classicFunctions
+    {
+        public static bool criticalLife(battler b)
+        {
+            return b.hp < (int)b.maxHp * 0.3;
+        }
+
+        public static int killableTarget(battler[] btls, int act, bool con1, bool frontTargets)
+        {
+            // Looking for enemies
+            for (int i = con1 ? 4 : 0; i < (con1 ? 8 : 4); i++)
+            {
+                if ( !btls[i].dead && (!btls[act].front || btls[i].front || !frontTargets))
+                {
+                    int dmg = combat_methods.damageOT(btls[act], btls[i].Clone());
+                    if (dmg >= btls[i].hp)
+                        return i;
+                }
+            }
+
+            // Return no possible target
+            return -1;
+        }
+
+        public static int greatestDamageTarget(battler[] btls, int act, bool con1, bool frontTargets)
+        {
+            int maxDamage = 0;
+            int maxTarget = -1;
+
+            // Looking for enemies
+            for (int i = con1 ? 4 : 0; i < (con1 ? 8 : 4); i++)
+            {
+                if (!btls[i].dead && (!btls[act].front || btls[i].front || !frontTargets))
+                {
+                    int dmg = combat_methods.damageOT(btls[act], btls[i].Clone());
+                    if (dmg >= maxDamage)
+                    {
+                        maxDamage = dmg;
+                        maxTarget = i;
+                    }
+
+                }
+            }
+
+            return maxTarget;
+        }
+    }
+}
